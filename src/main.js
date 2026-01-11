ef9ef43c77ea511c8142d7e8185327b1d398f712
ekimroyrp <ekimroyrp@gmail.com>
2026-01-11 11:44:10 -0800

    Adjust label offsets
    src/main.js: increase side/back/bottom label normal offsets

--- src/main.js
+++ src/main.js
@@ -458,9 +458,9 @@ function createFace(
 }
 
 const faces = {
-  bottom: createFace("Bottom", "bottom", -1, -1, 0, true, 0.06),
-  back: createFace("Back", "back", -1, 1, 0, true, 0.06),
-  side: createFace("Side", "side", -1, 1, 0, true, 0.06)
+  bottom: createFace("Bottom", "bottom", -1, -1, 0, true, 0.08),
+  back: createFace("Back", "back", -1, 1, 0, true, 0.08),
+  side: createFace("Side", "side", -1, 1, 0, true, 0.08)
 };
 
 faces.bottom.mesh.rotation.x = -Math.PI / 2;
